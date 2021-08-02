898d9b1fbd37291e15c59ba9a2b370acb2293f0d
zzh14266 <83206330+zzh14266@users.noreply.github.com>
2021-08-02 11:09:47 +0800

    。

--- src/main/java/com/turingit/drivingLicense/service/SavePhotoService_old.java
+++ src/main/java/com/turingit/drivingLicense/service/SavePhotoService_old.java
@@ -1,26 +1,26 @@
-package com.turingit.drivingLicense.service;
-
-import com.turingit.drivingLicense.pojo.Checkdata;
-import com.turingit.drivingLicense.pojo.ImageData;
-import com.turingit.drivingLicense.pojo.Summarizing;
-
-import java.util.List;
-
-public interface SavePhotoService_old {
-
-    String updataData(Long id);
-
-    List<ImageData> getError(Long id);
-
-    List getIdList(String st, String et);
-
-    Summarizing getImageData(int id);
-
-    void updateImage(Summarizing summarizing);
-
-    void updateImageNo(Long id);
-
-    Integer getSun();
-
-    List<ImageData> getErrorUp(Long firstId);
-}
+//package com.turingit.drivingLicense.service;
+//
+//import com.turingit.drivingLicense.pojo.Checkdata;
+//import com.turingit.drivingLicense.pojo.ImageData;
+//import com.turingit.drivingLicense.pojo.Summarizing;
+//
+//import java.util.List;
+//
+//public interface SavePhotoService_old {
+//
+//    String updataData(Long id);
+//
+//    List<ImageData> getError(Long id);
+//
+//    List getIdList(String st, String et);
+//
+//    Summarizing getImageData(int id);
+//
+//    void updateImage(Summarizing summarizing);
+//
+//    void updateImageNo(Long id);
+//
+//    Integer getSun();
+//
+//    List<ImageData> getErrorUp(Long firstId);
+//}
